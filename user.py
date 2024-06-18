92a471517aa2e7a32b810de56c67d6f7904207a7
Alex Urapin <kulob1@mail.ru>
2024-06-18 11:46:10 +0400

    Initial commit

---
+++ user.py
@@ -0,0 +1,68 @@
+"""
+Напишите класс User, имеющий следующие свойства и методы:
+
+- __init__(self, name, password): конструктор, принимающий имя пользователя и пароль
+- name: свойство, которое возвращает имя пользователя
+- password: свойство, которое позволяет установить или изменить пароль пользователя
+- is_admin: свойство, которое возвращает, является ли пользователь администратором или нет
+- _is_admin: свойство-помощник, которое определяет, является ли пользователь администратором или нет
+- login(self, password): метод, который проверяет, соответствует ли введенный пароль паролю пользователя
+- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False при условии, что пользователь залогинен)
+
+Для свойств name и password используйте декораторы @property и @password.setter.
+"""
+
+
+class User:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        self.status = False
+        self.is_logged_in = True
+        self.is_admin = False
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password_new(self, value):
+        self.password = value
+
+    @property
+    def is_admin(self):
+        return self.status
+
+    @is_admin.setter
+    def _is_admin(self, value):
+        self.status = value
+
+    def login(self, password):
+        if password == self.password:
+            return True
+        return False
+
+    def logout(self):
+        if self.is_logged_in == True:
+            self.is_logged_in = False
+
+
+
+# код для проверки
+user1 = User("Alice", "qwerty")
+print(user1.name)  # Alice
+print(user1.password)  # qwerty
+print(user1.is_admin)  # False
+
+user1.password = "newpassword"
+print(user1.password)  # newpassword
+
+user1._is_admin = True
+print(user1.is_admin)  # True
+
+user1.login("newpassword")  # True
+user1.logout()
